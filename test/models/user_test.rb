84f3886502c4868e23eb832129129c223178e4d6
Lawrence Kroll <larry43.lk@gmail.com>
2021-02-22 12:00:20 +0900

    model testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "test that the method #full_name returns a nicely formatted name" do
+    # setup
+    user = User.new(first_name: "larry", last_name: "mafia", email: "mafia@hll.com")
+    # verify
+    assert_equal 'Larry Mafia', user.full_name
+  end
 end
